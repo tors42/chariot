ed70658a4063f7585a50524afc38bb9ebd731a78
tors42 <4084220+tors42@users.noreply.github.com>
2021-09-10 17:27:22 +0200

    Format fixes Example.java

--- build/Example.java
+++ build/Example.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 
 class Example {
 
-    static String defaultTeamId = "lichess-swiss"; // https://lichess.org/team/lichess-swiss
+    // https://lichess.org/team/lichess-swiss
+    static String defaultTeamId = "lichess-swiss";
 
     public static void main(String... args) {
         String teamId = Arrays.stream(args).findFirst().orElse(defaultTeamId);
@@ -13,13 +14,17 @@ class Example {
 
         if (result.isPresent()) {
             var team = result.get();
-            System.out.println(team.name() + " has " + team.nbMembers() + " members!");
+            System.out.format(
+                    "%s has %d members!%n",
+                    team.name(), team.nbMembers());
         } else {
-            System.out.format("""
-                    Couldn't find the team with team id "%s"
-                    Note, a team id should be all lowercase and instead of whitespace there are dashes.
-                    The team "Lichess Swiss" for instance, has the team id "lichess-swiss".
-                    """, teamId);
+            System.out.format(
+                """
+                Couldn't find the team with team id "%s"
+                Note, a team id should be all lowercase and instead of
+                whitespace there are dashes. The team "Lichess Swiss" for
+                instance, has the team id "lichess-swiss".
+                """, teamId);
         }
     }
 }
