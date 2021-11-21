f1d621fe154307869eb8203687f81e3366ab205d
tors42 <4084220+tors42@users.noreply.github.com>
2021-11-21 23:15:19 +0100

    Update tablebase result model

--- src/main/chariot/chariot/model/TablebaseResult.java
+++ src/main/chariot/chariot/model/TablebaseResult.java
@@ -3,26 +3,28 @@ package chariot.model;
 import java.util.List;
 
 public record TablebaseResult(
-        Integer wdl,
         Integer dtz,
+        Integer precise_dtz,
         Integer dtm,
         boolean checkmate,
         boolean stalemate,
         boolean insufficient_material,
         boolean variant_win,
         boolean variant_loss,
+        String category,
         List<Move> moves) implements Model {
 
     public record Move(
             String uci,
             String san,
-            Integer wdl,
             Integer dtz,
+            Integer precise_dtz,
             Integer dtm,
             boolean zeroing,
             boolean checkmate,
             boolean stalemate,
             boolean variant_win,
             boolean variant_loss,
-            boolean insufficient_material) {}
+            boolean insufficient_material,
+            String category) {}
 }
