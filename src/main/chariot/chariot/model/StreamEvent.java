c601335568eaf6078e037e8482d75db74f8691dc
tors42 <4084220+tors42@users.noreply.github.com>
2022-02-05 14:46:10 +0100

    Add additional info for game on game event

--- src/main/chariot/chariot/model/StreamEvent.java
+++ src/main/chariot/chariot/model/StreamEvent.java
@@ -1,5 +1,12 @@
 package chariot.model;
 
+import java.util.Optional;
+
+import chariot.model.Enums.Color;
+import chariot.model.Enums.PerfType;
+import chariot.model.Enums.Speed;
+import chariot.model.Enums.VariantName;
+
 public sealed interface StreamEvent extends Model {
 
     enum Type { gameStart, gameFinish, challenge, challengeCanceled, challengeDeclined }
@@ -9,13 +16,13 @@ public sealed interface StreamEvent extends Model {
 
         // --enable-preview
         // return switch(this) {
-        //     case GameEvent ge -> ge.game().id();
+        //     case GameEvent ge -> ge.game().gameId();
         //     case ChallengeEvent ce -> ce.challenge().id();
         //     case ChallengeAIEvent cae -> cae.challenge().id();
         // };
 
         if (this instanceof GameEvent ge) {
-            return ge.game().id();
+            return ge.game().gameId();
         } else if (this instanceof ChallengeEvent ce) {
             return ce.challenge().id();
         } else if (this instanceof ChallengeAIEvent cae) {
@@ -25,8 +32,31 @@ public sealed interface StreamEvent extends Model {
         }
     }
 
-    record GameEvent(Type type, Game game) implements StreamEvent {
-        public record Game(String id, String source, Compat compat) {}
+    record GameEvent(Type type, GameInfo game) implements StreamEvent {
+        public record GameInfo(
+                String fullId,
+                String gameId,
+                String fen,
+                Color color,
+                String lastMove,
+                Variant variant,
+                Speed speed,
+                PerfType perf,
+                boolean rated,
+                boolean hasMoved,
+                Opponent opponent,
+                boolean isMyTurn,
+                int secondsLeft,
+                Optional<String> tournamentId,
+                Optional<String> swissId,
+                String source,
+                Compat compat
+                ) {}
+        public record Variant (VariantName key, String name) {}
+        public sealed interface Opponent permits Opponent.User, Opponent.AI {
+            public record User (String id, String username, Integer rating) implements Opponent {}
+            public record AI(Integer ai) implements Opponent {}
+        }
     }
 
     // Todo, check what challenge structure is used in this StreamEvent - maybe something similar generic (as opposed to identical separate structurs)
