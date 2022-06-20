af2d3ff7098970191a0d8bef9b49a2fd07c06798
tors42 <4084220+tors42@users.noreply.github.com>
2022-06-20 02:40:24 +0200

    Fix nowPlaying model
    Resolves: #3

--- src/main/chariot/chariot/model/NowPlaying.java
+++ src/main/chariot/chariot/model/NowPlaying.java
@@ -1,20 +1,25 @@
 package chariot.model;
 
+import java.util.List;
+
 import chariot.model.Enums.Color;
 
-public record NowPlaying(
-        String fullId,
-        String gameId,
-        String fen,
-        Color color,
-        String lastMove,
-        String speed,
-        String perf,
-        boolean rated,
-        boolean isMyTurn,
-        Variant variant,
-        Opponent opponent) implements Model {
-
+public record NowPlaying(List<Game> nowPlaying) implements Model {
+    public record Game(
+            String fullId,
+            String gameId,
+            String fen,
+            Color color,
+            String lastMove,
+            String source,
+            String speed,
+            String perf,
+            Integer secondsLeft,
+            boolean rated,
+            boolean isMyTurn,
+            boolean hasMoved,
+            Variant variant,
+            Opponent opponent) {}
     public record Variant(String key, String name) {}
     public record Opponent(String id, String username, Integer rating) {}
 }
