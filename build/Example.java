9d8a675370b7a68149ea52309585d735db27fa1f
tors42 <4084220+tors42@users.noreply.github.com>
2022-07-21 15:29:06 +0200

    Update Example.java
    - Use ifPresentOrElse
    - Add example with stream() for results with many entries

--- build/Example.java
+++ build/Example.java
@@ -1,7 +1,7 @@
 //DEPS io.github.tors42:chariot:0.0.42
 //JAVA 17+
 import chariot.Client;
-import java.util.Arrays;
+import java.util.*;
 
 class Example {
 
@@ -12,21 +12,24 @@ class Example {
         String teamId = Arrays.stream(args).findFirst().orElse(defaultTeamId);
 
         var client = Client.basic();
-        var result = client.teams().byTeamId(teamId);
 
-        if (result.isPresent()) {
-            var team = result.get();
-            System.out.format(
-                    "%s has %d members!%n",
-                    team.name(), team.nbMembers());
-        } else {
-            System.out.format(
-                """
-                Couldn't find the team with team id "%s"
-                Note, a team id should be all lowercase and instead of
-                whitespace there are dashes. The team "Lichess Swiss" for
-                instance, has the team id "lichess-swiss".
-                """, teamId);
+        client.teams().byTeamId(teamId).ifPresentOrElse(
+                team -> System.out.format("%s has %d members%n", team.name(), team.nbMembers()),
+                () -> System.out.format("""
+                    Couldn't find the team with team id "%s"
+                    Note, a team id should be all lowercase and instead of
+                    whitespace there are dashes. The team "Lichess Swiss" for
+                    instance, has the team id "lichess-swiss".
+                    """, teamId));
+
+        List<String> members = client.teams().usersByTeamId(teamId).stream()
+            .limit(3)
+            .map(user -> user.username())
+            .toList();
+
+        if (! members.isEmpty()) {
+            System.out.println("\nSome members in the team:");
+            members.forEach(System.out::println);
         }
     }
 }
