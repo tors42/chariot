f128f4078a0da63454821d86009663beaa7245b3
tors42 <4084220+tors42@users.noreply.github.com>
2023-01-18 18:45:41 +0100

    Build concurrently
    Shaving off a second or two

--- build/Build.java
+++ build/Build.java
@@ -5,32 +5,37 @@ import java.nio.file.*;
 import java.time.*;
 import java.time.format.*;
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.spi.*;
 
 class Build {
 
-    public static void main(String... args) throws Exception {
-        String module = "chariot";
-        String version = args.length > 0 ? args[0] : "0.0.1-SNAPSHOT";
+    public static void main(String[] args) throws Exception {
+        String version   = args.length > 0 ? args[0] : "0.0.1-SNAPSHOT";
         String timestamp = args.length > 1 ? args[1] : ZonedDateTime.now()
             .withNano(0).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+
+        build(version, timestamp);
+    }
+
+    static void build(String version, String timestamp) throws Exception {
+        String module = "chariot";
         String filenamePrefix = module + "-" + version;
 
-        var javac = ToolProvider.findFirst("javac").orElseThrow();
-        var jar = ToolProvider.findFirst("jar").orElseThrow();
+        var javac   = ToolProvider.findFirst("javac").orElseThrow();
+        var jar     = ToolProvider.findFirst("jar").orElseThrow();
         var javadoc = ToolProvider.findFirst("javadoc").orElseThrow();
 
         Path out = Path.of("out");
         del(out);
 
         Path moduleSrc = Path.of("src", "main");
-        Path classes = out.resolve("classes");
+        Path classes   = out.resolve("classes");
         Path moduleOut = out.resolve("modules");
-        Path metaInf = out.resolve("META-INF");
-        Path manifest = out.resolve("MANIFEST.MF");
+        Path metaInf   = out.resolve("META-INF");
+        Path manifest  = out.resolve("MANIFEST.MF");
 
-        for (var dir : List.of(moduleOut, metaInf))
-            Files.createDirectories(dir);
+        for (var dir : List.of(moduleOut, metaInf)) Files.createDirectories(dir);
 
         Files.copy(Path.of("LICENSE"), metaInf.resolve("LICENSE"));
 
@@ -40,14 +45,16 @@ class Build {
                 Created-By: %s
                 """.formatted(module, version, Runtime.version()));
 
-        run(javac,
+        var executor = Executors.newCachedThreadPool();
+
+        executor.submit(() -> {
+            run(javac,
                 "--release", "17",
                 "--module-source-path", moduleSrc,
                 "--module", module,
                 "-d", classes
-           );
-
-        run(jar,
+               );
+            run(jar,
                 "--create",
                 "--date", timestamp,
                 "--manifest", manifest,
@@ -55,40 +62,45 @@ class Build {
                 "--file", moduleOut.resolve(filenamePrefix + ".jar"),
                 "-C", out, "META-INF",
                 "-C", classes.resolve(module), "."
-           );
+               );
+        });
 
-        run(javadoc,
+        executor.submit(() -> {
+            run(javadoc,
                 "--release", "17",
                 "-notimestamp",
                 "--module-source-path", moduleSrc,
                 "--module", module,
                 "-d", out.resolve("javadoc")
-           );
-
-        run(jar,
+               );
+            run(jar,
                 "--create",
                 "--date", timestamp,
                 "--manifest", manifest,
                 "--file", out.resolve(filenamePrefix + "-javadoc.jar"),
                 "-C", out, "META-INF",
                 "-C", out.resolve("javadoc"), "."
-           );
+               );
+        });
 
-        run(jar,
+        executor.submit(() ->
+            run(jar,
                 "--create",
                 "--date", timestamp,
                 "--manifest", manifest,
                 "--file", out.resolve(filenamePrefix + "-sources.jar"),
                 "-C", out, "META-INF",
                 "-C", moduleSrc, "."
-           );
+           ));
+
+        executor.shutdown();
     }
 
     static void del(Path dir) {
         if (dir.toFile().exists()) {
             try (var files = Files.walk(dir)) {
-                files.sorted(Collections.reverseOrder()).map(Path::toFile).forEach(f -> f.delete());
-            } catch (Exception e) {}
+                files.sorted(Collections.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            } catch (Exception e) { throw new RuntimeException(e); }
         }
     }
 
