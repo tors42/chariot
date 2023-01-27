835db1a6b773a19a579ea5716fa0ae0e9b27cba1
tors42 <4084220+tors42@users.noreply.github.com>
2023-01-27 19:07:00 +0100

    Update Studies parameters
    Add source()
    Add orientation()
    JavaDoc

--- src/main/chariot/chariot/api/Studies.java
+++ src/main/chariot/chariot/api/Studies.java
@@ -22,12 +22,44 @@ public interface Studies {
     }
 
     interface Params {
+        /**
+         * Include clock comments in the PGN moves, when available.
+         * Example: {@code 2. exd5 { [%clk 1:01:27] } e5 { [%clk 1:01:28] }}
+         * Default: true
+         */
         Params clocks(boolean clocks);
         default Params clocks() { return clocks(true); }
+        /**
+         * Include analysis and annotator comments in the PGN moves, when available.
+         * Example: {@code 12. Bxf6 { [%eval 0.23] } a3 { White is in a pickle. }}
+         * Default: true
+         */
         Params comments(boolean comments);
         default Params comments() { return comments(true); }
+
+        /**
+         * Include non-mainline moves, when available.
+         * Example: {@code 4. d4 Bb4+ (4... Nc6 5. Nf3 Bb4+ 6. Bd2 (6. Nbd2 O-O 7. O-O) 6... Bd6) 5. Nd2}
+         * Default: true
+         */
         Params variations(boolean variations);
         default Params variations() { return variations(true); }
+
+        /**
+         * Add a `Source` PGN tag with the study chapter URL.
+         * Example: {@code [Source "https://lichess.org/study/4NBHImfM/1Tk4IyTz"]}
+         * Default: false
+         */
+        Params source(boolean source);
+        default Params source() { return source(true); }
+
+        /**
+         * Add a `Orientation` PGN tag with the chapter predefined orientation.
+         * Example: {@code [Orientation "white"]}
+         * Default: false
+         */
+        Params orientation(boolean orientation);
+        default Params orientation() { return orientation(true); }
     }
 
 }
