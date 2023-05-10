6309a28763475b3602d1c97da66dd3666a540947
tors42 <4084220+tors42@users.noreply.github.com>
2023-05-10 18:55:37 +0200

    Add ZonedDateTime model parsing

--- src/main/chariot/chariot/internal/yayson/YayMapper.java
+++ src/main/chariot/chariot/internal/yayson/YayMapper.java
@@ -7,6 +7,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.RecordComponent;
 import java.lang.reflect.Type;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -16,6 +17,7 @@ import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import chariot.internal.Util;
 import chariot.internal.yayson.Parser.YayNode;
 import chariot.internal.yayson.Parser.YayNode.*;
 import chariot.internal.yayson.Parser.YayNode.YayValue.*;
@@ -259,6 +261,14 @@ public class YayMapper {
             }
             return null;
         } else {
+
+            if (ZonedDateTime.class.equals(cls)) {
+                if (node instanceof YayValue.YayNumber number) {
+                    ZonedDateTime zdt = Util.fromLong(number.value().longValue());
+                    return cls.cast(zdt);
+                }
+            }
+
             T t = buildFromPrimitive(node, cls);
             return t;
         }
