c38371ba9f7f74d8cc91818ad809713817870e77
tors42 <4084220+tors42@users.noreply.github.com>
2024-01-12 17:05:11 +0100

    Broadcast field "boolean official" -> "int tier"
    0 -> no tier
    3 -> normal
    4 -> high
    5 -> best

--- src/main/chariot/chariot/model/Broadcast.java
+++ src/main/chariot/chariot/model/Broadcast.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public record Broadcast(Tour tour, List<Round> rounds)  {
-    public record Tour(String id, String name, String slug, String description, ZonedDateTime createdAt, boolean official, String markup, URI url) {}
+    public record Tour(String id, String name, String slug, String description, ZonedDateTime createdAt, int tier, String markup, URI url) {}
     public record Round(String id, String name, String slug, ZonedDateTime createdAt, boolean ongoing, boolean finished, Optional<ZonedDateTime> startsAt, RoundTour tour, URI url) {}
-    public record RoundTour(String id, String name, String slug, String description, ZonedDateTime createdAt, boolean official) {}
+    public record RoundTour(String id, String name, String slug, String description, ZonedDateTime createdAt, int tier) {}
 }
--- src/main/chariot/chariot/model/MyRound.java
+++ src/main/chariot/chariot/model/MyRound.java
@@ -6,7 +6,7 @@ import java.time.ZonedDateTime;
 import java.util.Optional;
 
 public record MyRound(Tour tour, Round round, Study study) {
-    public record Tour(String id, String slug, String name, String description, ZonedDateTime createdAt, boolean official) {}
+    public record Tour(String id, String slug, String name, String description, ZonedDateTime createdAt, int tier) {}
     public record Round(String id, String slug, String name, ZonedDateTime createdAt, Optional<ZonedDateTime> startsAt, boolean ongoing, boolean finished, URI url, Duration delay) {}
     public record Study(boolean writeable) {}
 }
