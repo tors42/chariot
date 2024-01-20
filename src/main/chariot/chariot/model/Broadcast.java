68b6328ddfeeedaeecc63de9d4cceba177582176
tors42 <4084220+tors42@users.noreply.github.com>
2024-01-20 18:52:30 +0100

    Add .id() for broadcast/round "containers"

--- src/main/chariot/chariot/model/Broadcast.java
+++ src/main/chariot/chariot/model/Broadcast.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 public record Broadcast(Tour tour, List<Round> rounds)  {
+
+    public String id() { return tour().id(); }
+
     public record Tour(String id, String name, String slug, String description, ZonedDateTime createdAt, int tier, String markup, URI url) {}
     public record Round(String id, String name, String slug, ZonedDateTime createdAt, boolean ongoing, boolean finished, Optional<ZonedDateTime> startsAt, RoundTour tour, URI url) {}
     public record RoundTour(String id, String name, String slug, String description, ZonedDateTime createdAt, int tier) {}
--- src/main/chariot/chariot/model/MyRound.java
+++ src/main/chariot/chariot/model/MyRound.java
@@ -6,6 +6,9 @@ import java.time.ZonedDateTime;
 import java.util.Optional;
 
 public record MyRound(Tour tour, Round round, Study study) {
+
+    public String id() { return round().id(); }
+
     public record Tour(String id, String slug, String name, String description, ZonedDateTime createdAt, int tier) {}
     public record Round(String id, String slug, String name, ZonedDateTime createdAt, Optional<ZonedDateTime> startsAt, boolean ongoing, boolean finished, URI url, Duration delay) {}
     public record Study(boolean writeable) {}
