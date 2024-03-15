274ccbf186665f71d92dfd19eb0edfde0041933d
tors42 <4084220+tors42@users.noreply.github.com>
2024-03-15 19:42:13 +0100

    Add challenge id getter
    (refactor to exhaustive switch when bumping Java version)

--- src/main/chariot/chariot/model/Challenge.java
+++ src/main/chariot/chariot/model/Challenge.java
@@ -8,6 +8,14 @@ public sealed interface Challenge permits
     Challenge.DeclinedChallenge,
     Challenge.RematchChallenge {
 
+        default String id() {
+            if (this instanceof ChallengeInfo info)           return info.id();
+            if (this instanceof ChallengeWithRules withRules) return withRules.challenge().id();
+            if (this instanceof DeclinedChallenge declined)   return declined.challenge().id();
+            if (this instanceof RematchChallenge rematch)     return rematch.challenge().id();
+            return "";
+        }
+
         record ChallengeWithRules(List<String> rules, Challenge challenge) implements Challenge {}
         record RematchChallenge(String rematchOf, Challenge challenge) implements Challenge {}
         record DeclinedChallenge(String key, String reason, Challenge challenge) implements Challenge {}
