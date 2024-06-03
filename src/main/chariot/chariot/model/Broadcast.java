2143f0dd0b45c85d0659f78b17d1b30801538df7
tors42 <4084220+tors42@users.noreply.github.com>
2024-06-03 18:22:16 +0200

    Add .id() helper, and switch to Opt

--- src/main/chariot/chariot/model/Broadcast.java
+++ src/main/chariot/chariot/model/Broadcast.java
@@ -3,16 +3,17 @@ package chariot.model;
 import java.net.URI;
 import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public record Broadcast(Tour tour, List<Round> rounds)  {
     public String id() { return tour().id(); }
 
     public record Tour(String id, String name, String slug, String description, ZonedDateTime createdAt, int tier, String markup, URI url, Opt<URI> image, boolean teamTable, boolean leaderboard) {}
-    public record Round(String id, String name, String slug, ZonedDateTime createdAt, boolean ongoing, boolean finished, Optional<ZonedDateTime> startsAt, RoundTour tour, URI url) {}
+    public record Round(String id, String name, String slug, ZonedDateTime createdAt, boolean ongoing, boolean finished, Opt<ZonedDateTime> startsAt, RoundTour tour, URI url) {}
     public record RoundTour(String id, String name, String slug, String description, ZonedDateTime createdAt, int tier) {}
 
 
-    public record TourWithLastRound(Tour tour, RoundByUser lastRound) {}
-    public record RoundByUser(String id, String name, String slug, ZonedDateTime createdAt, boolean ongoing, boolean finished, Optional<ZonedDateTime> startsAt, URI url) {}
+    public record TourWithLastRound(Tour tour, RoundByUser lastRound, Opt<String> group) {
+        public String id() { return tour.id(); }
+    }
+    public record RoundByUser(String id, String name, String slug, ZonedDateTime createdAt, boolean ongoing, boolean finished, Opt<ZonedDateTime> startsAt, URI url) {}
 }
