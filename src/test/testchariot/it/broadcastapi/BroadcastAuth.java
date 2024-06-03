e28e46c3ca47a9f14b3849123b0acf9b4c84b371
tors42 <4084220+tors42@users.noreply.github.com>
2024-06-03 20:00:21 +0200

    IT - increase broadcast creation test

--- src/test/testchariot/it/broadcastapi/BroadcastAuth.java
+++ src/test/testchariot/it/broadcastapi/BroadcastAuth.java
@@ -1,15 +1,21 @@
 package it.broadcastapi;
 
 import chariot.ClientAuth;
+import chariot.model.*;
 import util.IntegrationTest;
 import util.IT;
 
-import static util.Assert.assertTrue;
-import static util.Assert.unboxEquals;
+import static util.Assert.*;
+
+import java.net.URI;
+import java.time.*;
+import java.util.List;
+import java.util.Locale;
 
 public class BroadcastAuth {
 
     static ClientAuth client = IT.diego();
+    static ClientAuth superadmin = IT.superadmin();
 
     @IntegrationTest
     public void createSimpleBroadcast() {
@@ -17,16 +23,103 @@ public class BroadcastAuth {
         String descrShort = "Simple short description";
         String descrLong = "Simple long description";
 
+        ZonedDateTime create = ZonedDateTime.now();
         var broadcastRequest = client.broadcasts().create(p -> p
                 .name(name)
                 .shortDescription(descrShort)
                 .longDescription(descrLong));
 
-        unboxEquals(broadcastRequest, name,       b -> b.tour().name());
-        unboxEquals(broadcastRequest, descrShort, b -> b.tour().description());
-        // "<p>Simple long description<p>\n"
-        assertTrue(broadcastRequest.map(b -> b.tour().markup()).orElse("").contains(descrLong));
+        if (! (broadcastRequest instanceof Entry(Broadcast broadcast))) {
+            fail("Couldn't create broadcast " + broadcastRequest);
+            return;
+        }
+
+        String id = broadcast.id();
+        ZonedDateTime createdAt = broadcast.tour().createdAt();
+
+        String slug = name.toLowerCase(Locale.ROOT).replace(' ', '-');
+        Opt<URI> image = Opt.empty();
+        String markup = "<p>" + descrLong + "</p>\n";
+
+        assertTrue(Duration.between(create, broadcast.tour().createdAt()).toMillis() < 100, "Created At off");
+
+        int tier = 0;
+        boolean teamTable = false;
+        boolean leaderboard = false;
+
+        List<Broadcast.Round> rounds = List.of();
+
+        Broadcast expected = new Broadcast(
+                new Broadcast.Tour(
+                        id,
+                        name,
+                        slug,
+                        descrShort,
+                        createdAt,
+                        tier,
+                        markup,
+                        IT.lilaURI().resolve("/broadcast/" + slug + "/" + id),
+                        image,
+                        teamTable,
+                        leaderboard),
+                rounds);
+
+        assertEquals(expected, broadcast);
     }
+
+    @IntegrationTest
+    public void createBroadcastWithAllParameters() {
+        String name = "Advanced Broadcast";
+        String descrShort = "Advanced short description";
+        String descrLong = "Advanced long description";
+        int tier = 4;
+        boolean autoLeaderboard = true;
+        boolean teamLeaderboard = true;
+
+        ZonedDateTime create = ZonedDateTime.now();
+        var broadcastRequest = superadmin.broadcasts().create(p -> p
+                .name(name)
+                .shortDescription(descrShort)
+                .longDescription(descrLong)
+                .tier(tier)
+                .autoLeaderboard(autoLeaderboard)
+                .teamTable(teamLeaderboard));
+
+        if (! (broadcastRequest instanceof Entry(Broadcast broadcast))) {
+            fail("Couldn't create broadcast " + broadcastRequest);
+            return;
+        }
+
+        String id = broadcast.id();
+        ZonedDateTime createdAt = broadcast.tour().createdAt();
+
+        String slug = name.toLowerCase(Locale.ROOT).replace(' ', '-');
+        Opt<URI> image = Opt.empty();
+        String markup = "<p>" + descrLong + "</p>\n";
+
+        assertTrue(Duration.between(create, broadcast.tour().createdAt()).toMillis() < 100, "Created At off");
+
+
+        List<Broadcast.Round> rounds = List.of();
+
+        Broadcast expected = new Broadcast(
+                new Broadcast.Tour(
+                        id,
+                        name,
+                        slug,
+                        descrShort,
+                        createdAt,
+                        tier,
+                        markup,
+                        IT.lilaURI().resolve("/broadcast/" + slug + "/" + id),
+                        image,
+                        teamLeaderboard,
+                        autoLeaderboard),
+                rounds);
+
+        assertEquals(expected, broadcast);
+    }
+
 
     @IntegrationTest
     public void createSimpleRound() {
@@ -36,7 +129,11 @@ public class BroadcastAuth {
                 .longDescription("Broadcast long description (markup) IT"));
 
         String roundName = "Simple Round";
+
+        //client.logging(l -> l.response().all());
         var roundRequest = broadcastResult.map(broadcast -> client.broadcasts().createRound(broadcast.id(), p -> p.name(roundName))).get();
+        //client.logging(l -> l.response().off());
+
         unboxEquals(roundRequest, roundName, r -> r.round().name());
     }
 }
