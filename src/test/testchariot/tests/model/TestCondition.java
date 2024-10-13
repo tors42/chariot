d345af2cff2daccdc7309efc53adde18157c9e86
tors42 <4084220+tors42@users.noreply.github.com>
2024-10-13 12:47:06 +0200

    Add teamname -> teamid test

---
+++ src/test/testchariot/tests/model/TestCondition.java
@@ -0,0 +1,26 @@
+package tests.model;
+
+import static util.Assert.assertEquals;
+
+import java.util.Map;
+import chariot.model.Condition;
+import util.Test;
+
+public class TestCondition {
+
+    @Test
+    public void parseTeamNameToTeamId() {
+        Map.of(
+                "Simple",                  "simple",
+                "Thor - Love and Blunder", "thor-love-and-blunder"
+              )
+            .forEach( (exampleTeamName, expectedTeamId) -> assertEquals(expectedTeamId, parseViaCondition(exampleTeamName)));
+    }
+
+    static String parseViaCondition(String teamName) {
+        return switch (Condition.memberByTeamName(teamName)) {
+            case Condition.Member(String teamId) -> teamId;
+            default -> "Failed to parse [%s]".formatted(teamName);
+        };
+    }
+}
