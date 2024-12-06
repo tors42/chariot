eb91aab290fc4a49f405f8a9b2f06525232918f4
tors42 <4084220+tors42@users.noreply.github.com>
2024-12-06 21:10:31 +0100

    Fix Puzzle model

--- src/main/chariot/chariot/model/Puzzle.java
+++ src/main/chariot/chariot/model/Puzzle.java
@@ -5,9 +5,8 @@ import java.util.List;
 import chariot.model.Enums.Color;
 
 public record Puzzle(GameInfo game, PuzzleInfo puzzle)  {
-    public record GameInfo(String id, String clock, String pgn, boolean rated, Perf perf, List<Player> players) {
-        public record Perf(String name, String icon) {}
-        public record Player(Color color, String name, String userId) {}
-    }
+    public record GameInfo(String id, String clock, String pgn, boolean rated, Perf perf, List<Player> players) {}
+    public record Perf(String name, String key) {}
+    public record Player(Color color, String name, String id, boolean patron, int rating, Opt<String> title, Opt<String> flair) {}
     public record PuzzleInfo(String id, int initialPly, int rating, int plays, List<String> solution, List<String> themes) {}
 }
