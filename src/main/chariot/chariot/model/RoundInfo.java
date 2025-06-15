57b923d7cedd111ef4a0e43632bfc282e01b475f
tors42 <4084220+tors42@users.noreply.github.com>
2025-06-15 18:04:49 +0200

    Update RoundInfo model
    RoundInfo.Round - remove and reuse Broadcast.Round instead
    RoundInfo.Tour
     + info
     + url
     + dates
    RoundInfo.Game
     + check
     / Opt thinkTime
    RoundInfo.Player
     + Opt fideId
     + Opt team

--- src/main/chariot/chariot/model/RoundInfo.java
+++ src/main/chariot/chariot/model/RoundInfo.java
@@ -4,14 +4,16 @@ import java.net.URI;
 import java.time.ZonedDateTime;
 import java.util.List;
 
-public record RoundInfo(Round round, Tour tour, Study study, List<Game> games, Opt<Broadcast.Group> group)  {
+public record RoundInfo(Broadcast.RoundByUser round, Tour tour, Study study, List<Game> games, Opt<Broadcast.Group> group)  {
 
     public String id() { return round().id(); }
 
-    public record Round(String id, String name, String slug, ZonedDateTime createdAt, boolean ongoing, boolean finished, Opt<ZonedDateTime> startsAt, URI url) {}
-    public record Tour(String id, String name, String slug, String description, ZonedDateTime createdAt, int tier, Opt<URI> image) {}
+    // Note, Tour teamTable is not present available in the endpoint this model is used,
+    // so sadly can't reuse Broadcast.Tour - because a missing teamTable field here does not mean `false` - it is just not known...
+    public record Tour(String id, String name, String slug, Opt<Broadcast.Info> info, ZonedDateTime createdAt, URI url, int tier, List<ZonedDateTime> dates, Opt<URI> image) {}
+
     public record Study(boolean writeable) {}
 
-    public record Game(String id, String name, String fen, List<Player> players, String lastMove, int thinkTime, String status) {}
-    public record Player(String name, Opt<String> title, Opt<Integer> rating, int clock, Opt<String> fed) {}
+    public record Game(String id, String name, String fen, List<Player> players, String lastMove, String check, Opt<Integer> thinkTime, String status) {}
+    public record Player(String name, Opt<String> title, Opt<Integer> rating, Opt<Integer> fideId, Opt<String> team, Opt<String> fed, int clock) {}
 }
