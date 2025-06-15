0149d7bba76ed365eac712ed05b7952f44e1689a
tors42 <4084220+tors42@users.noreply.github.com>
2025-06-15 13:28:27 +0200

    Format

--- src/main/chariot/chariot/model/Broadcast.java
+++ src/main/chariot/chariot/model/Broadcast.java
@@ -7,22 +7,65 @@ import java.util.List;
 public record Broadcast(Tour tour, List<Round> rounds, Opt<Group> group)  {
     public String id() { return tour().id(); }
 
-    public record Tour(String id, String name, String slug, ZonedDateTime createdAt, List<ZonedDateTime> dates, Info info, int tier, String description, URI url, Opt<URI> image, boolean teamTable) {
+    public record Tour(
+            String id,
+            String name,
+            String slug,
+            ZonedDateTime createdAt,
+            List<ZonedDateTime> dates,
+            Info info,
+            int tier,
+            String description,
+            URI url,
+            Opt<URI> image,
+            boolean teamTable
+            ) {
         public Opt<ZonedDateTime> startDate() { return dates().isEmpty() == false ? Opt.of(dates().getFirst()) : Opt.empty(); }
         public Opt<ZonedDateTime> endDate() { return dates().size() == 2  ? Opt.of(dates().getLast()) : Opt.empty(); }
     }
-    public record Round(String id, String name, String slug, ZonedDateTime createdAt, boolean ongoing, boolean finished, boolean startsAfterPrevious, Opt<ZonedDateTime> startsAt, Opt<ZonedDateTime> finishedAt, Opt<RoundTour> tour, URI url) {}
-    public record RoundTour(String id, String name, String slug, String description, ZonedDateTime createdAt, int tier) {}
+    public record Round(
+            String id,
+            String name,
+            String slug,
+            ZonedDateTime createdAt,
+            boolean ongoing,
+            boolean finished,
+            boolean startsAfterPrevious,
+            Opt<ZonedDateTime> startsAt,
+            Opt<ZonedDateTime> finishedAt,
+            Opt<RoundTour> tour,
+            URI url
+            ) {}
 
+    public record RoundTour(String id, String name, String slug, String description, ZonedDateTime createdAt, int tier) {}
 
     public record TourWithLastRound(Tour tour, RoundByUser round, Opt<String> group) {
         public String id() { return tour.id(); }
     }
-    public record RoundByUser(String id, String name, String slug, ZonedDateTime createdAt, boolean ongoing, boolean finished, Opt<ZonedDateTime> startsAt, URI url) {}
+
+    public record RoundByUser(
+            String id,
+            String name,
+            String slug,
+            ZonedDateTime createdAt,
+            boolean ongoing,
+            boolean finished,
+            Opt<ZonedDateTime> startsAt,
+            URI url
+            ) {}
 
     public record Group(String name, List<IdAndName> tours) {}
 
-    public record Info(Opt<String> format, Opt<String> tc, Opt<String> players, Opt<FideTC> fideTc, Opt<String> location, Opt<URI> website, Opt<URI> standings, Opt<String> timeZone) {
+    public record Info(
+            Opt<String> format,
+            Opt<String> tc,
+            Opt<String> players,
+            Opt<FideTC> fideTc,
+            Opt<String> location,
+            Opt<URI> website,
+            Opt<URI> standings,
+            Opt<String> timeZone
+            ) {
         public Info() {
             this(Opt.of(), Opt.of(), Opt.of(), Opt.of(), Opt.of(), Opt.of(), Opt.of(), Opt.of());
         }
--- src/main/chariot/chariot/model/MyRound.java
+++ src/main/chariot/chariot/model/MyRound.java
@@ -8,7 +8,29 @@ public record MyRound(Tour tour, Round round, Study study) {
 
     public String id() { return round().id(); }
 
-    public record Tour(String id, String slug, String name, Broadcast.Info info, ZonedDateTime createdAt, int tier, Opt<URI> image) {}
-    public record Round(String id, String slug, String name, ZonedDateTime createdAt, boolean startsAfterPrevious, Opt<ZonedDateTime> startsAt, Opt<ZonedDateTime> finishedAt, boolean ongoing, boolean finished, URI url, Duration delay) {}
+    public record Tour(
+            String id,
+            String slug,
+            String name,
+            Broadcast.Info info,
+            ZonedDateTime createdAt,
+            int tier,
+            Opt<URI> image
+            ) {}
+
+    public record Round(
+            String id,
+            String slug,
+            String name,
+            ZonedDateTime createdAt,
+            boolean startsAfterPrevious,
+            Opt<ZonedDateTime> startsAt,
+            Opt<ZonedDateTime> finishedAt,
+            boolean ongoing,
+            boolean finished,
+            URI url,
+            Duration delay
+            ) {}
+
     public record Study(boolean writeable) {}
 }
