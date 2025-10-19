a8c7581b6279f38291e7f60cdebc281334052d29
tors42 <4084220+tors42@users.noreply.github.com>
2025-10-19 13:39:19 +0200

    Add features to broadcast round info

--- src/chariot/chariot/model/RoundInfo.java
+++ src/chariot/chariot/model/RoundInfo.java
@@ -12,7 +12,8 @@ public record RoundInfo(Broadcast.RoundByUser round, Tour tour, Study study, Lis
     // so sadly can't reuse Broadcast.Tour - because a missing teamTable field here does not mean `false` - it is just not known...
     public record Tour(String id, String name, String slug, Opt<Broadcast.Info> info, ZonedDateTime createdAt, URI url, int tier, List<ZonedDateTime> dates, Opt<URI> image) {}
 
-    public record Study(boolean writeable) {}
+    public record Features(boolean chat, boolean computer, boolean explorer) {}
+    public record Study(boolean writeable, Features features) {}
 
     public record Game(String id, String name, String fen, List<Player> players, String lastMove, String check, Opt<Integer> thinkTime, String status) {}
     public record Player(String name, Opt<String> title, Opt<Integer> rating, Opt<Integer> fideId, Opt<String> team, Opt<String> fed, int clock) {}
