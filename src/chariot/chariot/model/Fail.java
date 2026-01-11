02f2cc11ce632b7d746520a0cddb3614b7171acf
tors42 <4084220+tors42@users.noreply.github.com>
2026-01-11 14:44:37 +0100

    Override Fail#toString

--- src/chariot/chariot/model/Fail.java
+++ src/chariot/chariot/model/Fail.java
@@ -5,4 +5,5 @@ import java.util.Optional;
 public record Fail<T>(int status, String message) implements Ack, One<T>, Many<T> {
      @Override public Optional<T> maybe() { return Optional.empty(); }
      @Override public T get() { return null; }
+     @Override public String toString() { return status == -1 ? message : "%d - %s".formatted(status, message); }
 }
