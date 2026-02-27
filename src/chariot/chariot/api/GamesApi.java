72825a37746034a40712b82970e8604f4575ab00
tors42 <4084220+tors42@users.noreply.github.com>
2026-02-27 20:34:21 +0100

    doc: delayed 3 moves rather than time

--- src/chariot/chariot/api/GamesApi.java
+++ src/chariot/chariot/api/GamesApi.java
@@ -155,7 +155,7 @@ public interface GamesApi {
      * A description of the game is sent as a first message. Then a message is sent
      * each time a move is played. Finally a description of the game is sent when
      * it finishes, and the stream is closed.<br>
-     * Ongoing games are delayed by a few seconds ranging from 3 to 60 depending on the time control,<br>
+     * Ongoing games are delayed by 3 moves,<br>
      * as to prevent cheat bots from using this API.<br>
      * No more than 8 game streams can be opened at the same time from the same IP address.
      */
