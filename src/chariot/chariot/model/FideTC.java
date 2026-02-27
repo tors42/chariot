3035ac813311520993515e475e540c6129724ebd
tors42 <4084220+tors42@users.noreply.github.com>
2026-02-27 21:52:59 +0100

    bliz() -> blitz()

--- src/chariot/chariot/model/FideTC.java
+++ src/chariot/chariot/model/FideTC.java
@@ -5,7 +5,7 @@ public enum FideTC { standard, rapid, blitz;
     public interface Provider {
         default FideTC standard() { return standard; }
         default FideTC rapid()    { return rapid; }
-        default FideTC bliz()     { return blitz; }
+        default FideTC blitz()     { return blitz; }
     }
     public static Provider provider() {return new Provider(){};}
 }
