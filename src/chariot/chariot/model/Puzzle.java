c7e0597417a70b57cf5578f6098791d0283f4196
tors42 <4084220+tors42@users.noreply.github.com>
2026-05-23 13:26:02 +0200

    Add 'lastMove' and 'fen' to Puzzle

--- src/chariot/chariot/model/Puzzle.java
+++ src/chariot/chariot/model/Puzzle.java
@@ -10,5 +10,5 @@ public record Puzzle(GameInfo game, PuzzleInfo puzzle)  {
     public record Player(Color color, String name, String id, Opt<Integer> patronColor, int rating, Opt<String> title, Opt<String> flair) {
         public boolean patron() { return patronColor.isPresent(); }
     }
-    public record PuzzleInfo(String id, int initialPly, int rating, int plays, List<String> solution, List<String> themes) {}
+    public record PuzzleInfo(String id, int initialPly, int rating, int plays, String fen, String lastMove, List<String> solution, List<String> themes) {}
 }
